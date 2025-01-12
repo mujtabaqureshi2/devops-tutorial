c2d57e3cf28693053d0d18ab7ff7f7264f05c8fa
Mujtaba Qureshi <sp20m2be012@iub.edu.pk>
2025-01-13 00:44:06 +0500

    modified code

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var server = http.createServer(function (request, response) {
   response.end("Hello getintodevops.com\n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+const port = 8000;
+const host = '0.0.0.0';
+
+server.listen(port, host, () => {
+  console.log(`Server listening at http://${host}:${port}/`);
+});
